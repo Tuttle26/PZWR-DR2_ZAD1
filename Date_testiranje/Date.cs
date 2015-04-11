d03371c453490571fdb13d9c2ee20b74f3f65c82
Tuttle26 <irunji00@fesb.hr>
2015-04-12 00:46:50 +0200

    domaci

---
+++ Date_testiranje/Date.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateProject
+{
+    public class Date
+    {
+        private int mDay;
+        private int mMonth;
+        private int mYear;
+
+        public Date(int year, int month, int day)
+        {
+            _validate(year, month, day);
+            mDay = day;
+            mMonth = month;
+            mYear = year;
+        }
+
+        // staticke metode, neovisne o instanci
+        public static void _validate(int year, int month, int day)
+        {
+            // ne treba za null, jer je int
+            if (year < 0 || year > 2999) throw new InvalidProgramException("Wrong year range");
+            if (month < 1 || month > 12) throw new InvalidProgramException("Wrong month range");
+
+            if (month == 2)
+            {
+                if (isLeapYear(year))
+                {
+                    if (day < 1 || day > 29) throw new InvalidProgramException("Wrong day range");
+                }
+                else
+                {
+                    if (day < 1 || day > 28) throw new InvalidProgramException("Wrong day range");
+                }
+            }
+            else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+            {
+                if (day < 1 || day > 31) throw new InvalidProgramException("Wrong day range");
+            }
+            else
+            {
+                if (day < 1 || day > 30) throw new InvalidProgramException("Wrong day range");
+            }
+        }
+
+        public static Boolean isLeapYear(int year)
+        {
+            if (year % 4 != 0) return false;
+            else if (year % 100 != 0) return true;
+            else if (year % 400 != 0) return false;
+            else return true;
+        }
+
+        // metode objekta
+        public String getMonthName() 
+        {
+            return System.Globalization.CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(mMonth);
+        }
+        public int getNumberOfRemaingDaysInMonth()
+        {
+            if (mMonth == 2)
+            {
+                if (isLeapYear(mYear))
+                {
+                    return 29 - mDay;
+                }
+                else
+                {
+                    return 28 - mDay;
+                }
+            }
+            else if (mMonth == 1 || mMonth == 3 || mMonth == 5 || mMonth == 7 || mMonth == 8 || mMonth == 10 || mMonth == 12)
+            {
+                return 31 - mDay;
+            }
+            else
+            {
+                return 30 - mDay;
+            }
+        }
+    }
+}
---
+++ TestDate/UnitTest1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DateProject;
+
+namespace TestDate
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [ExpectedException(typeof(InvalidProgramException))]
+        public void testWrongDate1()
+        {
+            Date date = new Date(2000, 2, 31);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidProgramException))]
+        public void testWrongDate2()
+        {
+            Date date = new Date(-5, 2, 31);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(InvalidProgramException))]
+        public void testWrongDate3()
+        {
+            Date date = new Date(2000, 13, 31);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidProgramException))]
+        public void testValidate1()
+        {
+            Date._validate(2000, 2, 31);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidProgramException))]
+        public void testValidate2()
+        {
+            Date._validate(2001, 2, 29);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidProgramException))]
+        public void testValidate3()
+        {
+            Date._validate(2001, 1, 32); 
+        }
+        [TestMethod]
+        [ExpectedException(typeof(InvalidProgramException))]
+        public void testValidate4()
+        {
+            Date._validate(2000, 4, 31);
+        }
+
+        [TestMethod]
+        public void testIsLeapYear1()
+        {
+            Assert.AreEqual(true, Date.isLeapYear(2004));
+        }
+
+        [TestMethod]
+        public void testIsLeapYear2()
+        {
+            Assert.AreEqual(true, Date.isLeapYear(400));
+        }
+
+        [TestMethod]
+        public void testIsLeapYear3()
+        {
+            Assert.AreEqual(false, Date.isLeapYear(200));
+        }
+
+        [TestMethod]
+        public void testGetMonthName()
+        {
+            Date date = new Date(2010, 5, 15);
+            Assert.AreEqual("svibanj", date.getMonthName());
+        }
+
+        [TestMethod]
+        public void testGetNumberOfRemaingDaysInMonth1()
+        {
+            Date date = new Date(2010, 4, 1);
+            Assert.AreEqual(29, date.getNumberOfRemaingDaysInMonth());
+        }
+        [TestMethod]
+        public void testGetNumberOfRemaingDaysInMonth2()
+        {
+            Date date = new Date(2010, 5, 1);
+            Assert.AreEqual(30, date.getNumberOfRemaingDaysInMonth());
+        }
+        [TestMethod]
+        public void testGetNumberOfRemaingDaysInMonth3()
+        {
+            Date date = new Date(2004, 2, 1);
+            Assert.AreEqual(28, date.getNumberOfRemaingDaysInMonth());
+        }
+        [TestMethod]
+        public void testGetNumberOfRemaingDaysInMonth4()
+        {
+            Date date = new Date(2005, 2, 1);
+            Assert.AreEqual(27, date.getNumberOfRemaingDaysInMonth());
+        }
+    }
+}
